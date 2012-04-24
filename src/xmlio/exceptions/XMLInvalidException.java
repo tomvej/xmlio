88041ffb608995ea87d9dba09fc54fab5b3a9b7b
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2012-04-24 10:26:13 +0200

    Introduced XMLInvalidException.

---
+++ src/xmlio/exceptions/XMLInvalidException.java
@@ -0,0 +1,19 @@
+package xmlio.exceptions;
+
+import xmlio.ObjectFactory;
+
+/**
+ * Thrown when argument expected by {@link ObjectFactory#get(org.w3c.dom.Node)}
+ * is not created from XML document valid w.r.t schema associated schema.
+ * 
+ * @author <a href="mailto:xvejpust@fi.muni.cz">Tomáš Vejpustek</a>
+ */
+public class XMLInvalidException extends RuntimeException {
+	public XMLInvalidException(String message) {
+		super(message);
+	}
+
+	public XMLInvalidException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
