04815287365fa48f5f06ccc976630f203cef8268
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2012-04-27 15:39:49 +0200

    XML manipulation methds of StreamXMLResource moved into an util class.

--- src/xmlio/resource/StreamXMLResource.java
+++ src/xmlio/resource/StreamXMLResource.java
@@ -5,34 +5,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMResult;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import xmlio.XMLResource;
 import xmlio.exceptions.XMLException;
 import xmlio.exceptions.XMLExceptionType;
+import xmlio.utils.XMLUtils;
 
 /**
  * Stores and loads object of designated type to and from a stream. Implements
@@ -45,7 +26,6 @@ import xmlio.exceptions.XMLExceptionType;
  *            Type of objects for storing and loading.
  */
 public abstract class StreamXMLResource<T> implements XMLResource<T> {
-	private static final String			SCHEMA_XMLNS	= "http://www.w3.org/2001/XMLSchema";
 
 	private T							root;
 	private TransformationContext<T>	context;
@@ -91,85 +71,16 @@ public abstract class StreamXMLResource<T> implements XMLResource<T> {
 
 	@Override
 	public void load() throws XMLException {
-		Validator valid = getValidator();
+		Validator valid = XMLUtils.getValidator(context.getXMLSchema());
 		InputStream is = openIpnut();
 		try {
-			Document doc = parseDocument(is, getDocumentBuilder());
-			Document input = validateDocument(doc, valid);
+			Document doc = XMLUtils.parseDocument(is, XMLUtils.getDocumentBuilder());
+			Document input = XMLUtils.validateDocument(doc, valid);
 			input.normalize();
-			removeEmptyNodes(input);
+			XMLUtils.removeEmptyNodes(input);
 			setRoot(context.getObjectFactory().get(input.getDocumentElement()));
 		} finally {
 			closeStream(is);
-		}
-	}
-
-	private Validator getValidator() throws XMLException {
-		SchemaFactory schFact = SchemaFactory.newInstance(SCHEMA_XMLNS);
-		Schema schema;
-		try {
-			schema = schFact.newSchema(context.getXMLSchema());
-		} catch (SAXException saxe) {
-			throw new XMLException(XMLExceptionType.SCHEMA, saxe);
-		}
-		return schema.newValidator();
-	}
-
-	private DocumentBuilder getDocumentBuilder() throws XMLException {
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		dbf.setNamespaceAware(true);
-		try {
-			return dbf.newDocumentBuilder();
-		} catch (ParserConfigurationException pce) {
-			throw new XMLException(XMLExceptionType.UNDEFINED, pce);
-		}
-	}
-
-	private Document parseDocument(InputStream is, DocumentBuilder docBuild)
-			throws XMLException {
-		try {
-			return docBuild.parse(is);
-		} catch (SAXException saxe) {
-			throw new XMLException(XMLExceptionType.PARSE, saxe);
-		} catch (IOException ioe) {
-			throw new XMLException(XMLExceptionType.PARSE_IO, ioe);
-		}
-	}
-
-	private Document validateDocument(Document src, Validator valid)
-			throws XMLException {
-		DOMResult result = new DOMResult();
-		try {
-			valid.validate(new DOMSource(src), result);
-		} catch (SAXException saxe) {
-			throw new XMLException(XMLExceptionType.VALIDATION, saxe);
-		} catch (IOException ioe) {
-			throw new XMLException(XMLExceptionType.VALIDATION_IO, ioe);
-		}
-		return (Document) result.getNode();
-	}
-
-	private void removeEmptyNodes(Document target) throws XMLException {
-		/*
-		 * http://stackoverflow.com/questions/978810/how-to-strip-whitespace-only
-		 * -text-nodes-from-a-dom-before-serialization
-		 */
-		XPathFactory fact = XPathFactory.newInstance();
-		XPathExpression expr;
-		try {
-			expr = fact.newXPath().compile("//text()[normalize-space(.) = '']");
-		} catch (XPathExpressionException xpee) {
-			throw new XMLException(XMLExceptionType.UNDEFINED, xpee);
-		}
-		NodeList emptyNodes;
-		try {
-			emptyNodes = (NodeList) expr.evaluate(target, XPathConstants.NODESET);
-		} catch (XPathExpressionException xpee) {
-			throw new XMLException(XMLExceptionType.UNDEFINED, xpee);
-		}
-		for (int i = 0; i < emptyNodes.getLength(); i++) {
-			Node empty = emptyNodes.item(i);
-			empty.getParentNode().removeChild(empty);
 		}
 	}
 
@@ -178,14 +89,14 @@ public abstract class StreamXMLResource<T> implements XMLResource<T> {
 		if (root == null) {
 			throw new IllegalStateException("No object to be stored.");
 		}
-		Document doc = getDocumentBuilder().newDocument();
+		Document doc = XMLUtils.getDocumentBuilder().newDocument();
 		Element target = context.getXMLFactory().toXML(root, doc);
 		setNamespace(target);
 		doc.appendChild(target);
 
 		OutputStream os = openOutput();
 		try {
-			printDocument(getTransformer(), doc, os);
+			XMLUtils.printDocument(XMLUtils.getTransformer(), doc, os);
 		} finally {
 			closeStream(os);
 		}
@@ -193,27 +104,6 @@ public abstract class StreamXMLResource<T> implements XMLResource<T> {
 
 	private void setNamespace(Element target) {
 		target.setAttribute("xmlns", context.getXMLNamespace());
-	}
-
-	private Transformer getTransformer() throws XMLException {
-		TransformerFactory fact = TransformerFactory.newInstance();
-
-		try {
-			Transformer result = fact.newTransformer();
-			result.setOutputProperty(OutputKeys.INDENT, "yes");
-			return result;
-		} catch (TransformerConfigurationException tce) {
-			throw new XMLException(XMLExceptionType.UNDEFINED, tce);
-		}
-	}
-
-	private void printDocument(Transformer trans, Document doc, OutputStream os)
-			throws XMLException {
-		try {
-			trans.transform(new DOMSource(doc), new StreamResult(os));
-		} catch (TransformerException te) {
-			throw new XMLException(XMLExceptionType.UNDEFINED, te);
-		}
 	}
 
 	private void closeStream(Closeable stream) throws XMLException {
---
+++ src/xmlio/utils/XMLUtils.java
@@ -0,0 +1,128 @@
+package xmlio.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import xmlio.exceptions.XMLException;
+import xmlio.exceptions.XMLExceptionType;
+
+public class XMLUtils {
+	private static final String	SCHEMA_XMLNS	= "http://www.w3.org/2001/XMLSchema";
+
+	public static DocumentBuilder getDocumentBuilder() throws XMLException {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		try {
+			return dbf.newDocumentBuilder();
+		} catch (ParserConfigurationException pce) {
+			throw new XMLException(XMLExceptionType.UNDEFINED, pce);
+		}
+	}
+
+	public static Validator getValidator(URL schemaURL) throws XMLException {
+		SchemaFactory schFact = SchemaFactory.newInstance(SCHEMA_XMLNS);
+		Schema schema;
+		try {
+			schema = schFact.newSchema(schemaURL);
+		} catch (SAXException saxe) {
+			throw new XMLException(XMLExceptionType.SCHEMA, saxe);
+		}
+		return schema.newValidator();
+	}
+
+	public static Document parseDocument(InputStream is, DocumentBuilder docBuild)
+			throws XMLException {
+		try {
+			return docBuild.parse(is);
+		} catch (SAXException saxe) {
+			throw new XMLException(XMLExceptionType.PARSE, saxe);
+		} catch (IOException ioe) {
+			throw new XMLException(XMLExceptionType.PARSE_IO, ioe);
+		}
+	}
+
+	public static Document validateDocument(Document src, Validator valid)
+			throws XMLException {
+		DOMResult result = new DOMResult();
+		try {
+			valid.validate(new DOMSource(src), result);
+		} catch (SAXException saxe) {
+			throw new XMLException(XMLExceptionType.VALIDATION, saxe);
+		} catch (IOException ioe) {
+			throw new XMLException(XMLExceptionType.VALIDATION_IO, ioe);
+		}
+		return (Document) result.getNode();
+	}
+
+	public static void removeEmptyNodes(Document target) throws XMLException {
+		/*
+		 * http://stackoverflow.com/questions/978810/how-to-strip-whitespace-only
+		 * -text-nodes-from-a-dom-before-serialization
+		 */
+		XPathFactory fact = XPathFactory.newInstance();
+		XPathExpression expr;
+		try {
+			expr = fact.newXPath().compile("//text()[normalize-space(.) = '']");
+		} catch (XPathExpressionException xpee) {
+			throw new XMLException(XMLExceptionType.UNDEFINED, xpee);
+		}
+		NodeList emptyNodes;
+		try {
+			emptyNodes = (NodeList) expr.evaluate(target, XPathConstants.NODESET);
+		} catch (XPathExpressionException xpee) {
+			throw new XMLException(XMLExceptionType.UNDEFINED, xpee);
+		}
+		for (int i = 0; i < emptyNodes.getLength(); i++) {
+			Node empty = emptyNodes.item(i);
+			empty.getParentNode().removeChild(empty);
+		}
+	}
+
+	public static Transformer getTransformer() throws XMLException {
+		TransformerFactory fact = TransformerFactory.newInstance();
+
+		try {
+			Transformer result = fact.newTransformer();
+			result.setOutputProperty(OutputKeys.INDENT, "yes");
+			return result;
+		} catch (TransformerConfigurationException tce) {
+			throw new XMLException(XMLExceptionType.UNDEFINED, tce);
+		}
+	}
+
+	public static void printDocument(Transformer trans, Document doc, OutputStream os)
+			throws XMLException {
+		try {
+			trans.transform(new DOMSource(doc), new StreamResult(os));
+		} catch (TransformerException te) {
+			throw new XMLException(XMLExceptionType.UNDEFINED, te);
+		}
+	}
+
+}
