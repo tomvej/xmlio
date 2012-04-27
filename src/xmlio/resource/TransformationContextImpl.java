92486726685ac80a8a1a72f368e4b9dc2ce2197f
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2012-04-27 16:03:53 +0200

    TransformationContext now reads namespace name from schema.

--- src/xmlio/resource/TransformationContextImpl.java
+++ src/xmlio/resource/TransformationContextImpl.java
@@ -1,9 +1,18 @@
 package xmlio.resource;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.net.URL;
+
+import org.w3c.dom.Document;
 
 import xmlio.ObjectFactory;
 import xmlio.XMLFactory;
+import xmlio.exceptions.XMLException;
+import xmlio.exceptions.XMLExceptionType;
+import xmlio.utils.XMLUtils;
 
 public class TransformationContextImpl<T> implements TransformationContext<T> {
 	private XMLFactory<T>		xmlFact;
@@ -16,6 +25,14 @@ public class TransformationContextImpl<T> implements TransformationContext<T> {
 		objFact = objectFactory;
 		schema = schemaURL;
 		this.namespace = namespace;
+	}
+
+	public TransformationContextImpl(XMLFactory<T> xmlFactory, ObjectFactory<T> objectFactory, URL schemaURL)
+			throws XMLException {
+		xmlFact = xmlFactory;
+		objFact = objectFactory;
+		schema = schemaURL;
+		namespace = getNamespaceFromSchema(schemaURL);
 	}
 
 	@Override
@@ -38,4 +55,15 @@ public class TransformationContextImpl<T> implements TransformationContext<T> {
 		return schema;
 	}
 
+	public static String getNamespaceFromSchema(URL schemaURL)
+			throws XMLException {
+		try {
+			InputStream is = new FileInputStream(new File(schemaURL.toString()));
+			Document schema = XMLUtils.parseDocument(is, XMLUtils.getDocumentBuilder());
+			return schema.getDocumentElement().getAttribute("targetNamespace");
+		} catch (FileNotFoundException fnfe) {
+			throw new XMLException(XMLExceptionType.SRC_OPEN, fnfe);
+		}
+
+	}
 }
